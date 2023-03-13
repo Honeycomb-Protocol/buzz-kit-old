1b7952d6ddee95ac30f2d5e9e79d34e97517381e
Shayan M Hussain <shayanhussain48@gmail.com>
2023-03-13 21:07:17 +0500

    project name changes && precommit fixes

--- scripts/precommit.js
+++ scripts/precommit.js
@@ -2,7 +2,7 @@ const { execSync } = require("child_process");
 const fs = require("fs");
 
 // write the version in cargo.toml of all programs
-const packages = fs.readdirSync("packages");
+const packages = fs.readdirSync("./packages");
 packages.forEach((package) => {
   if (package === "idl") return;
   const cargoTomlPath = `programs/${package}/Cargo.toml`;
