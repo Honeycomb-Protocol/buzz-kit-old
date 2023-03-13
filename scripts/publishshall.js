7d7b467c49ecf52a0082e2c5f88744eb0cad5b2a
Shayan M Hussain <shayanhussain48@gmail.com>
2023-03-13 20:39:57 +0500

    publish & scripts

---
+++ scripts/publishshall.js
@@ -0,0 +1,17 @@
+const { execSync } = require("child_process");
+const fs = require("fs");
+
+console.log(process.env.NODE_AUTH_TOKEN);
+
+// write the version in cargo.toml of all programs
+const packages = fs.readdirSync("packages");
+packages.forEach((package) => {
+  if (package === "idl") return;
+  execSync(
+    `echo _authToken=${process.env.NODE_AUTH_TOKEN} >> .npmrc && npm publish --access public`,
+    {
+      cwd: `dist/${package}`,
+      stdio: "inherit",
+    }
+  );
+});
